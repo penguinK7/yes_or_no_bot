8f788c889cc2f6544bf7a6fad5082c4d89d0b684
Ksenia Klenkova <kllenkova7@gmail.com>
2025-01-20 18:51:35 +0300

    улучшения убрала лишнюю команду

--- bot.py
+++ bot.py
@@ -1,53 +1,50 @@
 import logging
 import os
 import requests
-
 from dotenv import load_dotenv
-from telebot import TeleBot, types
+from telebot import TeleBot
 
+# Загрузка переменных окружения
 load_dotenv()
 
+# Инициализация бота
 secret_token = os.getenv('TOKEN')
 bot = TeleBot(token=secret_token)
 
+# Настройка логирования
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
 )
 
+# URL для API
 URL = 'https://yesno.wtf/api'
 
 def get_gif_response():
+    """Получение ответа и GIF из API."""
     try:
-        response = requests.get(URL).json()
-        answer = response.get('answer')
-        gif_url = response.get('image')
-        return answer, gif_url
-    except Exception as error:
+        response = requests.get(URL)
+        response.raise_for_status()  # Проверка на успешность запроса
+        data = response.json()
+        return data.get('answer'), data.get('image')
+    except requests.RequestException as error:
         logging.error(f'Ошибка при запросе к API: {error}')
         return None, None
 
 @bot.message_handler(commands=['start'])
 def wake_up(message):
+    """Приветственное сообщение при запуске бота."""
     chat_id = message.chat.id
     name = message.from_user.first_name
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton('/question')
-    keyboard.add(button)
 
     bot.send_message(
         chat_id=chat_id,
-        text=f'Здравствуйте, {name}! Этот бот поможет принять вам жизненно важные решения.',
-        reply_markup=keyboard,
+        text=f'Здравствуйте, {name}! Этот бот поможет принять вам жизненно важные решения. Пожалуйста, задайте свой вопрос.',
     )
-
-@bot.message_handler(commands=['question'])
-def new_question(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Ждем ваш вопрос.")
 
 @bot.message_handler(content_types=['text'])
 def handle_question(message):
+    """Обработка текстовых сообщений от пользователя."""
     chat_id = message.chat.id
     answer, gif_url = get_gif_response()
 
@@ -58,6 +55,7 @@ def handle_question(message):
         bot.send_message(chat_id, 'Извините, произошла ошибка при получении ответа.')
 
 def main():
+    """Запуск бота."""
     bot.polling(none_stop=True)
 
 if __name__ == '__main__':
